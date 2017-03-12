9345ff1c81b7bb2ed49b928573ed188ac05e1396
Victor Gaultney <victor_gaultney@sil.org>
2017-03-12 23:49:33 +0000

    Adding notification message; Simplifying quoting

--- LoadUFOfamily.py
+++ LoadUFOfamily.py
@@ -5,7 +5,7 @@ Loads a family of UFOs. Choose the Regular face and all others in the same folde
 """
 """
 To Do:
-- add routine to fix up internal Glyphs data for each master after import
+- Add support for custom master sets
 """
 __copyright__ = 'Copyright (c) 2017, SIL International  (http://www.sil.org)'
 __license__ = 'Released under the MIT License (http://opensource.org/licenses/MIT)'
@@ -13,20 +13,20 @@ __author__ = 'Victor Gaultney'
 
 import os
 
-familymembers = ["Bold", "Italic", "BoldItalic"]
+familymembers = ['Bold', 'Italic', 'BoldItalic']
 
 # Open Dialog:
 fontfile = GetOpenFile()
 
-if fontfile[-11:] == "Regular.ufo":
+if fontfile[-11:] == 'Regular.ufo':
 	mainfont = GSFont(fontfile)
 	familypath = fontfile[:-11]
 	for s in familymembers:
-		addfontpath = familypath + s + ".ufo"
+		addfontpath = familypath + s + '.ufo'
 		if os.path.exists(addfontpath):
 			addfont = GSFont(addfontpath)
 			mainfont.addFontAsNewMaster_(addfont.masters[0])
 			mainfont.instances.append(addfont.instances[0].copy())
 	Glyphs.fonts.append(mainfont)
 else:
-	print "Please select a file with a name ending in Regular.ufo"
+	Message('Try again','Please select a file with a name ending in Regular.ufo',OKButton='OK')
