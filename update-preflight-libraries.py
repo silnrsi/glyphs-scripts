dbfe66bd33e467257de7bf7514f0b69f8d55a05f
Nicolas Spalinger <nicolas.spalinger@sil.org>
2022-02-28 22:23:03 -0600

    Added ufoLib2 master, fontTools main, glyphConstruction master (it's not available on pypi.org), an clearer bits of explanation for potential path issues.

--- update-preflight-libraries.py
+++ update-preflight-libraries.py
@@ -10,7 +10,8 @@ __license__ = 'Released under the MIT License (http://opensource.org/licenses/MI
 __author__ = 'Nicolas Spalinger'
 
 # using brew and sudo we expect launchers to be put into /usr/local/bin/ and the libs in /usr/local/lib
-# but if you install manually with --user you will need to add ~/Library/Python/3.*/bin to your PATH in ~/.bash_profile
+# but if you install manually with --user you will need to add the following to ~/.bash_profile:
+export PATH="$PATH:$HOME/Library/Python/3.9/bin"
 
 from subprocess import Popen, PIPE
 import GlyphsApp
@@ -34,7 +35,7 @@ tell application "Finder"
 
 		tell window 1
 
-	        	do script "which python3; python3 --version; python3 -m pip --version; sudo -H python3 -m pip uninstall --yes pysilfont glyphsLib fontTools mutatorMath defcon fontMath; sudo -H python3 -m pip install --upgrade --no-cache-dir git+https://github.com/silnrsi/pysilfont.git@master#egg=pysilfont git+https://github.com/googlefonts/GlyphsLib.git@main#egg=glyphsLib fontTools fs mutatorMath defcon fontMath ; echo 'Please check to make sure these dependencies have been installed correctly: defcon, fontMath, fontTools, glyphsLib, MutatorMath and pysilfont. Only these dependencies are needed for preflight, other libraries can be reported as missing without problems. '; psfversion"
+	        	do script "which python3; python3 --version; python3 -m pip --version; sudo -H python3 -m pip uninstall --yes pysilfont glyphsLib fontTools mutatorMath defcon fontMath; sudo -H python3 -m pip install --upgrade --no-cache-dir git+https://github.com/silnrsi/pysilfont.git@master#egg=pysilfont git+https://github.com/googlefonts/GlyphsLib.git@main#egg=glyphsLib git+https://github.com/fonttools/ufoLib2.git@master#egg=ufoLib2 git+https://github.com/fonttools/fonttools.git@main#egg=fontTools git+https://github.com/typemytype/glyphConstruction.git@master#egg=glyphConstruction fs mutatorMath defcon fontMath ; echo 'Please check to make sure these dependencies have been installed correctly: defcon, fontMath, fontTools, glyphConstruction, glyphsLib, MutatorMath, pysilfont and ufoLib2. Only these dependencies are needed for preflight, other libraries can be reported as missing without problems as they are used for other purposes. '; psfversion"
 
 		end tell
 
